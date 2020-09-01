2866939228ccba4eb10c2552650a6829cace6950
Bùi Xuân Thắng <70557093+xuanthangisme@users.noreply.github.com>
2020-09-01 14:56:04 +0700

    update component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  menus = [
+    {name:'Trang chủ',url:'#',disabled:false,child:null},
+    {name:'Các hãng sản xuất',disabled:false,url:'#',child:[{name:'Mercedes Benz',url:'#'},{name:'BMW',url:'#'},{name:'Audi',url:'#'},{name:'Lamborghini',url:'#'},{name:'Vinfast',url:'#'}]},
+    {name:'Blog',url:'#',disabled:false,child:null},
+    {name:'About us',url:'#',disabled:false,child:null}
+  ];
   constructor() { }
 
   ngOnInit(): void {
