f237434156fddee2fb52d6280efb42b16b4ce26c
Bùi Xuân Thắng <70557093+xuanthangisme@users.noreply.github.com>
2020-09-01 11:02:29 +0700

    update component

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component,AfterViewInit, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewInit {
+  title = 'mazlay';
+  constructor(private renderer: Renderer2) { }
+  ngAfterViewInit() { 
+    setTimeout(() => {
+      this.loadScripts();
+    }); 
+  }
+  public loadScripts() {
+    this.renderExternalScript('assets/js/main.js').onload = () => {
+    }
+  }
+  public renderExternalScript(src: string): HTMLScriptElement {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = src;
+    script.async = true;
+    script.defer = true;
+    this.renderer.appendChild(document.body, script);
+    return script;
+  }
+ 
+}
